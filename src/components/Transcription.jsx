d06a53b6b00d5d3de792a739c3b8c51fa8ff1050
frank1433 <70449962+frank1433@users.noreply.github.com>
2024-06-11 17:36:13 -0400

    Initial commit

---
+++ src/components/Transcription.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+
+
+export default function Transcription(props) {
+    const { textElement } = props
+
+    return (
+        <div>{textElement}</div>
+    )
+}
